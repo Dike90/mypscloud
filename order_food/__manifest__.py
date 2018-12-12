65cfb80d16104eefe6db3d1492a02af7a5a580b9
Dike90 <919055994@qq.com>
2018-12-12 17:40:18 +0800

    v0.2

--- order_food/__manifest__.py
+++ order_food/__manifest__.py
@@ -3,10 +3,10 @@
     'name': "order_food",
 
     'summary': """
-        内部订餐系统""",
+        订餐系统""",
 
     'description': """
-        仅供PS产品部内部使用
+        PS产品部内部使用的订餐系统
     """,
 
     'author': "DK",
@@ -15,8 +15,8 @@
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
     # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
+    'category': 'order food',
+    'version': '0.2',
 
     # any module necessary for this one to work correctly
     'depends': ['base'],
@@ -31,5 +31,5 @@
     'demo': [
         'demo/demo.xml',
     ],
-    'application':True,
+    'application': True,
 }
--- order_food/models/models.py
+++ order_food/models/models.py
@@ -1,32 +1,51 @@
 # -*- coding: utf-8 -*-
-
+import datetime
 from odoo import models, fields, api
+
+
+class food(models.Model):
+    _name = 'order_food.food'
+
+    def compute_today(self):
+        return datetime.datetime.today().weekday()
+
+    id = fields.Many2one('order_food.order')
+    name = fields.Char(string='菜名')
+    # name = fields.Many2one('order_food.order',string='菜名')
+    price = fields.Float(string='价格')
+    # stock = fields.Integer(string='库存')
+    food_date = fields.Selection(string='菜谱日期', selection=[(0, '星期一'), (1, '星期二'), (2, '星期三'),
+                                    (3, '星期四'), (4, '星期五'), (5, '星期六'), (6, '星期日')], default=compute_today)
+    description = fields.Text(string='描述')
+    food_image = fields.Binary()
+
 
 class order(models.Model):
     _name = 'order_food.order'
 
-    id = fields.Integer()
-    order_no = fields.Integer()
-    user_id = fields.Many2one('res.users', ondelete='set null', string="用户")
-    #shipping_id = fields.Many2one()
-    payment = fields.Float()
-    payment_time = fields.Datetime(string="支付时间")
-    payment_status = fields.Integer(string='支付状态')
+    # def get_order_no(self):
+    #     # 年月日时分秒
+    #     return datetime.datetime.now().strftime("%Y%m%d%H%M%S")
 
-class food_menu(models.Model):
-    _name = 'order_food.food_menu'
+    user_ids = fields.Many2one('res.users', default=lambda self: self.env.user)
+    # order_no = fields.Char(string='订单编号', required=True, default=get_order_no)
+    food_ids = fields.Many2many('order_food.food', 'id', string='点菜', domain=[('food_date', '=', datetime.datetime.today().weekday())])  # , domain=[('stock', '>', 0)]
+    # food_names = fields.One2many('order_food.food', 'name')
 
-    name = fields.Char(string='菜谱名称')
-    id = fields.Integer()
-    menu_date = fields.Date(string='日期')
-    food_ids = fields.One2many('order_food.food_info','id', string='菜品')
+    # 计算字段默认是不存储的，若需要存储需要加store=True
+    amount = fields.Float(string='金额', compute='_calc_amount', store=True)
+    payment_info = fields.Integer(string='支付信息')
+    state = fields.Selection(selection=[(0, '点餐'), (1, '付款')])
 
-class food_info(models.Model):
-    _name = 'order_food.food_info'
+    # 依赖装饰器
+    @api.depends('food_ids')
+    def _calc_amount(self):
+        for p in self.food_ids:
+            self.amount += p.price
 
-    id = fields.Integer()
-    name = fields.Char(string='菜名')
-    price = fields.Float(string='价格')
-    detials = fields.Text(string='菜品描述')
-    food_menu_id = fields.Many2one('order_food.food_menu')
+
+    #@api.depends('food_ids')
+    def order_food(self):
+        pass
+
 
