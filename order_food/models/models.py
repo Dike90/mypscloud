b3525ba31756f65fd04b17462a817cec72fbd78a
Dike90 <919055994@qq.com>
2018-12-15 16:04:33 +0800

    milestone1

--- order_food/models/models.py
+++ order_food/models/models.py
@@ -9,7 +9,6 @@ class food(models.Model):
     def compute_today(self):
         return datetime.datetime.today().weekday()
 
-    id = fields.Many2one('order_food.order')
     name = fields.Char(string='菜名')
     # name = fields.Many2one('order_food.order',string='菜名')
     price = fields.Float(string='价格')
@@ -17,7 +16,7 @@ class food(models.Model):
     food_date = fields.Selection(string='菜谱日期', selection=[(0, '星期一'), (1, '星期二'), (2, '星期三'),
                                     (3, '星期四'), (4, '星期五'), (5, '星期六'), (6, '星期日')], default=compute_today)
     description = fields.Text(string='描述')
-    food_image = fields.Binary()
+    food_image = fields.Binary(string='图片')
 
 
 class order(models.Model):
@@ -27,15 +26,24 @@ class order(models.Model):
     #     # 年月日时分秒
     #     return datetime.datetime.now().strftime("%Y%m%d%H%M%S")
 
-    user_ids = fields.Many2one('res.users', default=lambda self: self.env.user)
-    # order_no = fields.Char(string='订单编号', required=True, default=get_order_no)
-    food_ids = fields.Many2many('order_food.food', 'id', string='点菜', domain=[('food_date', '=', datetime.datetime.today().weekday())])  # , domain=[('stock', '>', 0)]
-    # food_names = fields.One2many('order_food.food', 'name')
+    def compute_today(self):
+        return datetime.datetime.today().weekday()
 
+    user_id = fields.Many2one('res.users', string="用户名称", default=lambda self: self.env.user, readonly=True)
+    # order_no = fields.Char(string='订单编号', required=True, default=get_order_no)
+    state = fields.Selection(selection=[(0, '点餐'), (1, '付款')])
+    food_ids = fields.Many2many('order_food.food', string='点菜')  # ,domain=[('food_date', '=', datetime.datetime.today().weekday())]
+    # food_names = fields.One2many('order_food.food', 'name')
+    week_of_today = fields.Integer(default=compute_today)
     # 计算字段默认是不存储的，若需要存储需要加store=True
     amount = fields.Float(string='金额', compute='_calc_amount', store=True)
     payment_info = fields.Integer(string='支付信息')
-    state = fields.Selection(selection=[(0, '点餐'), (1, '付款')])
+
+
+
+    @api.depends()
+    def _compute_today(self):
+        self.week_of_today = datetime.datetime.today().weekday()
 
     # 依赖装饰器
     @api.depends('food_ids')
@@ -46,6 +54,12 @@ class order(models.Model):
 
     #@api.depends('food_ids')
     def order_food(self):
-        pass
+        self.state = 1
 
-
+# class order_detail(models.Model):
+#     _name = 'order_food.detail'
+#
+#     food_id = fields.Many2one('order_food.food', string='菜名')
+#     order_id = fields.Many2one('order_food.order')
+#     quantity = fields.Integer(string='数量')
+#     line_no = fields.Integer(string='行号')
