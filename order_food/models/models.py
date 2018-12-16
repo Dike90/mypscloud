be818a5971d3985a87088f69e76c16376531ed56
Dike90 <919055994@qq.com>
2018-12-16 21:51:56 +0800

    milestone

--- order_food/models/models.py
+++ order_food/models/models.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import datetime
 from odoo import models, fields, api
-
+from PIL import Image
 
 class food(models.Model):
     _name = 'order_food.food'
@@ -11,6 +11,7 @@ class food(models.Model):
 
     name = fields.Char(string='菜名')
     # name = fields.Many2one('order_food.order',string='菜名')
+    order_detail_ids = fields.One2many('order_food.detail','food_id')
     price = fields.Float(string='价格')
     # stock = fields.Integer(string='库存')
     food_date = fields.Selection(string='菜谱日期', selection=[(0, '星期一'), (1, '星期二'), (2, '星期三'),
@@ -31,9 +32,11 @@ class order(models.Model):
 
     user_id = fields.Many2one('res.users', string="用户名称", default=lambda self: self.env.user, readonly=True)
     # order_no = fields.Char(string='订单编号', required=True, default=get_order_no)
-    state = fields.Selection(selection=[(0, '点餐'), (1, '付款')])
-    food_ids = fields.Many2many('order_food.food', string='点菜')  # ,domain=[('food_date', '=', datetime.datetime.today().weekday())]
+    detail_ids = fields.One2many('order_food.detail','order_id',string='订单明细')
+    state = fields.Selection(selection=[(0, '点餐'), (1, '付款')], default=0)
+    # food_ids = fields.Many2many('order_food.food', string='点菜')  # ,domain=[('food_date', '=', datetime.datetime.today().weekday())]
     # food_names = fields.One2many('order_food.food', 'name')
+    order_date = fields.Datetime(string='订单日期', required=True, readonly=True, default=fields.Datetime.now)
     week_of_today = fields.Integer(default=compute_today)
     # 计算字段默认是不存储的，若需要存储需要加store=True
     amount = fields.Float(string='金额', compute='_calc_amount', store=True)
@@ -46,20 +49,50 @@ class order(models.Model):
         self.week_of_today = datetime.datetime.today().weekday()
 
     # 依赖装饰器
-    @api.depends('food_ids')
+    @api.depends('detail_ids')
     def _calc_amount(self):
-        for p in self.food_ids:
-            self.amount += p.price
+        for p in self.detail_ids:
+            self.amount += p.sale_price
 
-
-    #@api.depends('food_ids')
+    @api.multi
     def order_food(self):
         self.state = 1
+        # return {
+        #     'name': '付款二维码窗口',
+        #     'type': 'ir.actions.act_window',
+        #     'res_model': 'order_food.qrimage',
+        #     'view_type': 'form',
+        #     'view_mode': 'form',
+        #     'target': 'self',
+        #     'view_id': 'order_food_qrimage_form.id',
+        # }
 
-# class order_detail(models.Model):
-#     _name = 'order_food.detail'
-#
-#     food_id = fields.Many2one('order_food.food', string='菜名')
-#     order_id = fields.Many2one('order_food.order')
-#     quantity = fields.Integer(string='数量')
-#     line_no = fields.Integer(string='行号')
+
+class order_detail(models.Model):
+    _name = 'order_food.detail'
+
+    food_id = fields.Many2one('order_food.food', string='菜名')
+    order_id = fields.Many2one('order_food.order')
+    quantity = fields.Integer(string='数量', default=1)
+    # readony=True，代码中也不可修改它的值,update也不行
+    sale_price = fields.Float(string='销售价格')
+    sequence = fields.Integer(string='行号', default=1)
+
+    @api.multi
+    @api.onchange('food_id')
+    def get_default_price(self):
+        print('trigger once')
+        print(self.food_id)
+        # vals={}
+        if self.food_id:
+            self.sale_price = self.food_id.price
+            # vals['sale_price'] = self.food_id.price
+            # self.update(vals)
+            print('updated')
+
+class pay_QRimage(models.Model):
+    _name = 'order_food.qrimage'
+
+    owner = fields.Many2one('res.users', string="二维码归属", default=lambda self: self.env.user)
+    QRimage = fields.Binary(string='付款二维码')
+
