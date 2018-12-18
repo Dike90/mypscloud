cc22833f587340f2a9f32547da14a48da1441dce
Dike90 <919055994@qq.com>
2018-12-18 08:45:12 +0800

    add QRimage

--- order_food/models/models.py
+++ order_food/models/models.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import datetime
 from odoo import models, fields, api
-from PIL import Image
+
 
 class food(models.Model):
     _name = 'order_food.food'
@@ -34,13 +34,13 @@ class order(models.Model):
     # order_no = fields.Char(string='订单编号', required=True, default=get_order_no)
     detail_ids = fields.One2many('order_food.detail','order_id',string='订单明细')
     state = fields.Selection(selection=[(0, '点餐'), (1, '付款')], default=0)
-    # food_ids = fields.Many2many('order_food.food', string='点菜')  # ,domain=[('food_date', '=', datetime.datetime.today().weekday())]
+    food_ids = fields.Many2many('order_food.food', string='点菜')  # ,domain=[('food_date', '=', datetime.datetime.today().weekday())]
     # food_names = fields.One2many('order_food.food', 'name')
     order_date = fields.Datetime(string='订单日期', required=True, readonly=True, default=fields.Datetime.now)
     week_of_today = fields.Integer(default=compute_today)
     # 计算字段默认是不存储的，若需要存储需要加store=True
     amount = fields.Float(string='金额', compute='_calc_amount', store=True)
-    payment_info = fields.Integer(string='支付信息')
+    payment_info = fields.Selection(string='所属部门',selection=[(0,'PS产品部'),(1,'战略产品部'),(2,'营销')])
 
 
 
@@ -57,6 +57,23 @@ class order(models.Model):
     @api.multi
     def order_food(self):
         self.state = 1
+
+        pay_info = self.env["order_food.qrimage"].search([("department", "=", self.payment_info)])
+        params = {'department': pay_info.department, 'image': pay_info.QRimage}
+
+        view_id = self.env['order_food.wizard']
+        new = view_id.create(params)
+
+        return {
+            'type': 'ir.actions.act_window',
+            'name': '付款码',
+            'res_model': 'order_food.wizard',
+            'view_type': 'form',
+            'view_mode': 'form',
+            'res_id': new.id,
+            'view_id': self.env.ref('order_food.my_order_food_wizard', False).id,
+            'target': 'new',
+        }
         # return {
         #     'name': '付款二维码窗口',
         #     'type': 'ir.actions.act_window',
@@ -81,18 +98,42 @@ class order_detail(models.Model):
     @api.multi
     @api.onchange('food_id')
     def get_default_price(self):
-        print('trigger once')
-        print(self.food_id)
+        # print('trigger once')
+        # print(self.food_id)
         # vals={}
         if self.food_id:
             self.sale_price = self.food_id.price
             # vals['sale_price'] = self.food_id.price
             # self.update(vals)
-            print('updated')
+            # print('updated')
 
 class pay_QRimage(models.Model):
     _name = 'order_food.qrimage'
 
+    department = fields.Selection(selection=[(0, 'PS产品部'), (1, '战略产品部'), (2, '营销')], index=True)
     owner = fields.Many2one('res.users', string="二维码归属", default=lambda self: self.env.user)
     QRimage = fields.Binary(string='付款二维码')
 
+
+    @api.one
+    def get_parms(self):
+        params = {'department': self.department, 'QRimage': self.QRimage}
+        return params
+
+
+    # @api.multi
+    # def action_confirm(self):
+    #     for order in self:
+    #         params=order.get_parms()
+    #         view_id=self.env['order_food.wizard']
+    #         new = view_id.create(params[0])
+    #         return {
+    #             'type': 'ir.actions.act_window',
+    #             'name': '付款码',
+    #             'res_model': 'order_food.wizard',
+    #             'view_type': 'form',
+    #             'view_mode': 'form',
+    #             'res_id'   : new.id,
+    #             'view_id': self.env.ref('control_credit_limit.my_order_food_wizard', False).id,
+    #             'target': 'new',
+    #         }
