62788868b2e344762f4ef854755626d36aba3159
Dike90 <919055994@qq.com>
2018-12-18 14:55:11 +0800

    基本功能都全了，就差按照部门查询了

--- order_food/models/models.py
+++ order_food/models/models.py
@@ -9,7 +9,7 @@ class food(models.Model):
     def compute_today(self):
         return datetime.datetime.today().weekday()
 
-    name = fields.Char(string='菜名')
+    name = fields.Char(string='菜名' , index=True)
     # name = fields.Many2one('order_food.order',string='菜名')
     order_detail_ids = fields.One2many('order_food.detail','food_id')
     price = fields.Float(string='价格')
@@ -19,6 +19,8 @@ class food(models.Model):
     description = fields.Text(string='描述')
     food_image = fields.Binary(string='图片')
 
+    _sql_constraints = [
+             ('食物唯一索引', 'unique (name)','不能创建重复的菜!')]
 
 class order(models.Model):
     _name = 'order_food.order'
@@ -34,13 +36,13 @@ class order(models.Model):
     # order_no = fields.Char(string='订单编号', required=True, default=get_order_no)
     detail_ids = fields.One2many('order_food.detail','order_id',string='订单明细')
     state = fields.Selection(selection=[(0, '点餐'), (1, '付款')], default=0)
-    food_ids = fields.Many2many('order_food.food', string='点菜')  # ,domain=[('food_date', '=', datetime.datetime.today().weekday())]
+    # food_ids = fields.Many2many('order_food.food', string='点菜')  # ,domain=[('food_date', '=', datetime.datetime.today().weekday())]
     # food_names = fields.One2many('order_food.food', 'name')
     order_date = fields.Datetime(string='订单日期', required=True, readonly=True, default=fields.Datetime.now)
     week_of_today = fields.Integer(default=compute_today)
     # 计算字段默认是不存储的，若需要存储需要加store=True
     amount = fields.Float(string='金额', compute='_calc_amount', store=True)
-    payment_info = fields.Selection(string='所属部门',selection=[(0,'PS产品部'),(1,'战略产品部'),(2,'营销')])
+    payment_info = fields.Selection(string='所属部门',selection=[(1,'PS产品部'),(2,'战略产品部'),(3,'营销')])
 
 
 
@@ -57,43 +59,46 @@ class order(models.Model):
     @api.multi
     def order_food(self):
         self.state = 1
+        if self.payment_info:
+            # print(self.payment_info)
+            pay_info = self.env["order_food.qrimage"].search([("department", "=", self.payment_info)])
+            params = {'department': pay_info.department, 'image': pay_info.QRimage}
+            # print(pay_info.QRimage)
+            view_id = self.env['order_food.wizard']
+            new = view_id.create(params)
 
-        pay_info = self.env["order_food.qrimage"].search([("department", "=", self.payment_info)])
-        params = {'department': pay_info.department, 'image': pay_info.QRimage}
-
-        view_id = self.env['order_food.wizard']
-        new = view_id.create(params)
-
-        return {
-            'type': 'ir.actions.act_window',
-            'name': '付款码',
-            'res_model': 'order_food.wizard',
-            'view_type': 'form',
-            'view_mode': 'form',
-            'res_id': new.id,
-            'view_id': self.env.ref('order_food.my_order_food_wizard', False).id,
-            'target': 'new',
-        }
-        # return {
-        #     'name': '付款二维码窗口',
-        #     'type': 'ir.actions.act_window',
-        #     'res_model': 'order_food.qrimage',
-        #     'view_type': 'form',
-        #     'view_mode': 'form',
-        #     'target': 'self',
-        #     'view_id': 'order_food_qrimage_form.id',
-        # }
+            return {
+                'type': 'ir.actions.act_window',
+                'name': '付款码',
+                'res_model': 'order_food.wizard',
+                'view_type': 'form',
+                'view_mode': 'form',
+                'res_id': new.id,
+                'view_id': self.env.ref('order_food.my_order_food_wizard', False).id,
+                'target': 'new',
+            }
+            # return {
+            #     'name': '付款二维码窗口',
+            #     'type': 'ir.actions.act_window',
+            #     'res_model': 'order_food.qrimage',
+            #     'view_type': 'form',
+            #     'view_mode': 'form',
+            #     'target': 'self',
+            #     'view_id': 'order_food_qrimage_form.id',
+            # }
 
 
 class order_detail(models.Model):
     _name = 'order_food.detail'
 
+    detail_date = fields.Datetime(readonly=True, default=fields.Datetime.now)
     food_id = fields.Many2one('order_food.food', string='菜名')
-    order_id = fields.Many2one('order_food.order')
+    order_id = fields.Many2one('order_food.order', ondelete='cascade')
     quantity = fields.Integer(string='数量', default=1)
-    # readony=True，代码中也不可修改它的值,update也不行
+    # readony=True，代码中也不可修改它的值,update也不行???
     sale_price = fields.Float(string='销售价格')
     sequence = fields.Integer(string='行号', default=1)
+
 
     @api.multi
     @api.onchange('food_id')
@@ -110,30 +115,12 @@ class order_detail(models.Model):
 class pay_QRimage(models.Model):
     _name = 'order_food.qrimage'
 
-    department = fields.Selection(selection=[(0, 'PS产品部'), (1, '战略产品部'), (2, '营销')], index=True)
+    department = fields.Selection(selection=[(1, 'PS产品部'), (2, '战略产品部'), (3, '营销')], index=True)
     owner = fields.Many2one('res.users', string="二维码归属", default=lambda self: self.env.user)
     QRimage = fields.Binary(string='付款二维码')
 
-
-    @api.one
-    def get_parms(self):
-        params = {'department': self.department, 'QRimage': self.QRimage}
-        return params
+    _sql_constraints = [
+             ('部门唯一索引', 'unique (department)','不能创建重复的部门!')]
 
 
-    # @api.multi
-    # def action_confirm(self):
-    #     for order in self:
-    #         params=order.get_parms()
-    #         view_id=self.env['order_food.wizard']
-    #         new = view_id.create(params[0])
-    #         return {
-    #             'type': 'ir.actions.act_window',
-    #             'name': '付款码',
-    #             'res_model': 'order_food.wizard',
-    #             'view_type': 'form',
-    #             'view_mode': 'form',
-    #             'res_id'   : new.id,
-    #             'view_id': self.env.ref('control_credit_limit.my_order_food_wizard', False).id,
-    #             'target': 'new',
-    #         }
+
